eaa1b16f7b36b968447d4b827618f65f2c2a7907
SAIKAT SAMANTA <saikat.samanta@hy-vee.com>
2025-09-04 21:04:47 +0530

    deployment ready

--- app/api/feedback/route.js
+++ app/api/feedback/route.js
@@ -1,7 +1,7 @@
 import { Resend } from 'resend';
 import { NextResponse } from 'next/server';
 
-const resend = new Resend('re_BM6W3bGx_3izx5awWEtjBQp9yE6e6MURS');
+const resend = process.env.RESEND_API_KEY ? new Resend(process.env.RESEND_API_KEY) : null
 
 export async function POST(request) {
   try {
